1ec8b6eb93077498c5ab8130aec7840490410bcd
joel <joelthobias447@gmail.com>
2022-11-22 23:03:35 +0530

    completed html part of input and output verified

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ let calccgpa = () => {
     let tcredit = 0;
     let tgpoint = 0;
     let cgpa = 0;
-    for (i = 0; i < 10; i ++) {
+    let cg=0;
+    for (i = 0; i < 9; i ++) {
         let cid = "cre" + i;
         let gid = "gra" + i;
-        let credit = parseInt(document.getElementById(cid).value)
-        let gradepoint = parseInt(document.getElementById(gid).value)
-        console.log(`credit =  ${cid}`);
-        cgpa += (credit * gradepoint)
+        let credit = parseFloat(document.getElementById(cid).value)
+        let gradepoint = parseFloat(document.getElementById(gid).value)
+        cg+=credit*gradepoint
+        console.log(`credit =  ${credit}`);
+        console.log(`gradepoint =  ${gradepoint}`);
+        console.log(`cg =  ${cg}`);
         tcredit += credit;
         tgpoint += gradepoint;
     }
-    document.getElementById('res').value = cgpa;
+    cgpa=cg/tcredit
+    console.log(`cgpa =  ${cgpa}`);
+    document.getElementById('res').innerHTML = cgpa;
 }
